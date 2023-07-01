7c9ef3116ffc63886ca11e0ae18c4c785264b7a6
erfanfi79 <erfanfaravani@gmail.com>
2023-07-02 00:00:56 +0330

    Fix Fitness functions

--- FitnessFunctions.py
+++ FitnessFunctions.py
@@ -1,7 +1,6 @@
-
-def min_completion_latency(task,start_time):
+def min_completion_latency(task, start_time):
     latency = task.get_latency(start_time)
-    completion_time = task.get_wait_time(start_time)
+    completion_time = task.get_completion_time(start_time)
     fitness_score = 1 / (0.6 * completion_time + 0.4 * latency)
     return fitness_score
 
@@ -11,6 +10,7 @@ def min_response_wait(task, start_time):
     wait_time = task.get_wait_time(start_time)
     fitness_score = 1 / (0.6 * response_time + 0.4 * wait_time)
     return fitness_score
+
 
 def min_wait_latency(task, start_time):
     latency = task.get_latency(start_time)
@@ -28,20 +28,23 @@ def min_response_latency(task, start_time):
 
 def min_completion_response(task, start_time):
     response_time = task.get_response_time(start_time)
-    completion_time = task.get_wait_time(start_time)
+    completion_time = task.get_completion_time(start_time)
     fitness_score = 1 / (0.6 * completion_time + 0.4 * response_time)
     return fitness_score
 
 
-def min_completion_sumOfSlack(task, start_time):
+def min_completion_sumOfSlack(tasks, task, start_time):
     pass
 
-
-def min_completion_latency_response(task, start_time):
+def min_completion_latency_response( task, start_time):
     latency = task.get_latency(start_time)
+    response_time = task.get_response_time(start_time)
+    completion_time = task.get_completion_time(start_time)
+    fitness_score = 1 / (0.3 * latency + 0.3 * completion_time + 0.5 * response_time)
+    return fitness_score
 
+
+def min_completion_sumOfSlack_response(tasks, task, start_time):
+    sumOfSlack = sum([t.get_slack_time(start_time) for t in tasks])
     pass
 
-
-def min_completion_sumOfSlack_response(task, start_time):
-    pass
--- Task.py
+++ Task.py
@@ -9,7 +9,7 @@ class Task:
         self.arrival_time = arrival_time
         self.deadline = deadline
         self.execution_time = execution_time
-        self.slack_time = self.deadline - self.arrival_time - self.execution_time
+        self.slack_time = 0
         self.start_time = 0
         self.finish_time = 0
         self.waiting_time = 0
@@ -17,6 +17,9 @@ class Task:
 
     def get_response_time(self, start_time):
         return self.get_completion_time(start_time) - self.arrival_time
+
+    def get_slack_time(self, start_time):
+        return max(self.deadline - self.get_completion_time(start_time), 0)
 
     def get_completion_time(self, start_time):
         return start_time + self.execution_time
@@ -26,5 +29,3 @@ class Task:
 
     def get_latency(self, start_time):
         return self.get_completion_time(start_time) - self.deadline
-
-
