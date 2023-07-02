cdedc58f9cba9a7a56183fa3d6f5810d937bd416
erfanfi79 <erfanfaravani@gmail.com>
2023-07-02 13:09:58 +0330

    Fix Fitness functions

--- FitnessFunctions.py
+++ FitnessFunctions.py
@@ -1,50 +1,117 @@
-def min_completion_latency(task, start_time):
-    latency = task.get_latency(start_time)
-    completion_time = task.get_completion_time(start_time)
+def min_completion_latency(schedule, tasks):
+    latency = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        task_latency = task.get_latency(time)
+        latency += task_latency
+        time += task.execution_time
+        if task_latency > 0:
+            time -= task_latency
+    completion_time = time
     fitness_score = 1 / (0.6 * completion_time + 0.4 * latency)
     return fitness_score
 
 
-def min_response_wait(task, start_time):
-    response_time = task.get_response_time(start_time)
-    wait_time = task.get_wait_time(start_time)
+def min_response_wait(schedule, tasks):
+    response_time = 0
+    wait_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        response_time += task.get_response_time(time)
+        wait_time += task.get_wait_time(time)
+        time += task.execution_time
+
     fitness_score = 1 / (0.6 * response_time + 0.4 * wait_time)
     return fitness_score
 
 
-def min_wait_latency(task, start_time):
-    latency = task.get_latency(start_time)
-    wait_time = task.get_wait_time(start_time)
+def min_wait_latency(schedule, tasks):
+    latency = 0
+    wait_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        latency += task.get_latency(time)
+        wait_time += task.get_wait_time(time)
+        time += task.execution_time
     fitness_score = 1 / (0.6 * wait_time + 0.4 * latency)
     return fitness_score
 
 
-def min_response_latency(task, start_time):
-    latency = task.get_latency(start_time)
-    response_time = task.get_response_time(start_time)
+def min_response_latency(schedule, tasks):
+    latency = 0
+    response_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        latency += task.get_latency(time)
+        response_time += task.get_response_time(time)
+        time += task.execution_time
     fitness_score = 1 / (0.7 * response_time + 0.3 * latency)
     return fitness_score
 
 
-def min_completion_response(task, start_time):
-    response_time = task.get_response_time(start_time)
-    completion_time = task.get_completion_time(start_time)
+def min_completion_response(schedule, tasks):
+    response_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        task_latency = task.get_latency(time)
+        response_time += task.get_response_time(time)
+        time += task.execution_time
+        if task_latency > 0:
+            time -= task_latency
+    completion_time = time
     fitness_score = 1 / (0.6 * completion_time + 0.4 * response_time)
     return fitness_score
 
 
-def min_completion_sumOfSlack(tasks, task, start_time):
-    pass
+def min_completion_sumOfSlack(schedule, tasks):
+    sumOfSlack = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        task_latency = task.get_latency(time)
+        sumOfSlack += task.get_slack_time(time)
+        time += task.execution_time
+        if task_latency > 0:
+            time -= task_latency
+    completion_time = time
+    fitness_score = 1 / (0.6 * completion_time + 0.4 * sumOfSlack)
+    return fitness_score
 
-def min_completion_latency_response( task, start_time):
-    latency = task.get_latency(start_time)
-    response_time = task.get_response_time(start_time)
-    completion_time = task.get_completion_time(start_time)
+
+def min_completion_latency_response(schedule, tasks):
+    latency = 0
+    response_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        task_latency = task.get_latency(time)
+        latency += task_latency
+        response_time += task.get_response_time(time)
+        time += task.execution_time
+        if task_latency > 0:
+            time -= task_latency
+    completion_time = time
     fitness_score = 1 / (0.3 * latency + 0.3 * completion_time + 0.5 * response_time)
     return fitness_score
 
 
-def min_completion_sumOfSlack_response(tasks, task, start_time):
-    sumOfSlack = sum([t.get_slack_time(start_time) for t in tasks])
-    pass
-
+def min_completion_sumOfSlack_response(schedule, tasks):
+    sumOfSlack = 0
+    response_time = 0
+    time = 0
+    for task_id in schedule:
+        task = tasks[task_id]
+        task_latency = task.get_latency(time)
+        sumOfSlack += task.get_slack_time(time)
+        response_time += task.get_response_time(time)
+        time += task.execution_time
+        if task_latency > 0:
+            time -= task_latency
+    completion_time = time
+    fitness_score = 1 / (0.3 * response_time + 0.3 * completion_time + 0.5 * response_time)
+    return fitness_score
