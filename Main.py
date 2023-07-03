f691b6f9aeb9039e2edaebdeea2212ab7a248d35
Alireza Ziaee <alrz1999@gmail.com>
2023-07-03 21:15:17 +0330

    Set task infoes

--- Main.py
+++ Main.py
@@ -22,11 +22,27 @@ def make_multicore_scheduling(num_cores, mapping_type, u_total, scheduling_type)
     cores = [Core(i) for i in range(num_cores)]
     task_instances, hyper_period = generate_tasks_instances(n=2, u_total=u_total)
     assign_tasks(cores, task_instances, hyper_period, mapping_type)
+    schedules = []
     if scheduling_type == SchedulingType.ABC:
         for core in cores:
-            ABC = ABC_Scheduler(len(core.assigned_tasks), 10, 10, 10, 10)
-            ABC.run(core.assigned_tasks)
+            ABC = ESA_Scheduler(len(core.assigned_tasks), 10, 10, 10)
+            schedule = ABC.run(core.assigned_tasks)
+            schedules.append(schedule)
+
+    for schedule in schedules:
+        current_time = 0
+        for task_index in schedule:
+            task = task_instances[task_index]
+            task.start_time = current_time
+            task.finish_time = current_time + task.execution_time
+            task.waiting_time = task.finish_time - task.execution_time - task.arrival_time
+            task.response_time = task.finish_time - task.arrival_time
+            task.slack_time = task.finish_time - task.execution_time - task.arrival_time
+
+            current_time += task.execution_time
+
+    print(task_instances[0])
 
 
 if __name__ == '__main__':
-    make_multicore_scheduling(5, MappingType.FIRST_FIT, 1, SchedulingType.ABC)
+    make_multicore_scheduling(2, MappingType.FIRST_FIT, 1, SchedulingType.ABC)
